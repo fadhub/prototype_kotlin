5f9c858b0f3a5ceb119a060be98879b4d5175cf8
fadhub <fadna.lakhouchen@gmail.com>
2025-10-08 13:23:09 +0100

    live Coding prototype is add

---
+++ liveCoding_prototype/app/src/main/java/com/example/livecoding_prototype/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.livecoding_prototype
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.MaterialTheme
+import com.example.livecoding_prototype.ui.NameListScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MaterialTheme {
+                NameListScreen()
+            }
+        }
+    }
+}
+
---
+++ liveCoding_prototype/app/src/main/java/com/example/livecoding_prototype/ui/Person.kt
@@ -0,0 +1,3 @@
+package com.example.livecoding_prototype.ui
+
+data class Person(val name: String)
