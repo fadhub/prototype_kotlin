3e416203b6146e3debc33848d325f13f51814819
fadhub <fadna.lakhouchen@gmail.com>
2025-10-10 14:58:30 +0100

    projet realisation is add

--- liveCoding_prototype/app/src/main/java/com/example/livecoding_prototype/ui/MembersScreen.kt
+++ liveCoding_prototype/app/src/main/java/com/example/livecoding_prototype/ui/MembersScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
+
 @Composable
 fun NameListScreen() {
 
